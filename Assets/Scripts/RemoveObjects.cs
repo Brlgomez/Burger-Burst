c0c82b685f238ef2a15b3853f5f2884cc035d946
Brandon Gomez <brlgomez@ucsc.edu>
2017-09-17 15:10:45 -0700

    Can now add items on only the waiter

--- Assets/Scripts/RemoveObjects.cs
+++ Assets/Scripts/RemoveObjects.cs
@@ -9,19 +9,26 @@ public class RemoveObjects : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
-        Debug.Log(col.gameObject.name);
         if ((col.gameObject.tag == "Building" || col.gameObject.tag == "Table") && gameObject.tag != "Fallen" && gameObject.tag != "OnPlatter")
         {
             gameObject.tag = "Fallen";
             gameObject.AddComponent<ShrinkObject>();
             Camera.main.GetComponent<Gameplay>().IncreaseNumberOfLostProduct(gameObject);
             Destroy(gameObject, lifetime);
+            Destroy(GetComponent<RemoveObjects>());
         } 
         else if (col.gameObject.tag == "Waiter")
         {
-            GameObject.Find("Waiter").GetComponent<Waiter>().AddToPlatter(gameObject);
-            transform.parent = col.gameObject.transform;
-            Destroy(GetComponent<RemoveObjects>());
+            Vector3 closestPointOnItem = gameObject.GetComponent<MeshCollider>().ClosestPoint(col.gameObject.transform.position);
+            Vector3 closestPointOnWaiter = col.gameObject.GetComponent<Collider>().ClosestPoint(closestPointOnItem);
+            //Debug.Log("POINT: " + closestPointOnWaiter);
+            //Debug.Log("DISTANCE: " + Vector3.Distance(closestPointOnItem, closestPointOnWaiter));
+            if (Vector3.Distance(closestPointOnItem, closestPointOnWaiter) < 0.1f)
+            {
+                GameObject.Find("Waiter").GetComponent<Waiter>().AddToPlatter(gameObject);
+                transform.parent = col.gameObject.transform;
+                Destroy(GetComponent<RemoveObjects>());
+            }
         }
     }
 }
