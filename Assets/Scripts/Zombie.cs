bc4a203faaef3c5d162e364c7b31c0e47331245f
Brandon Gomez <brlgomez@ucsc.edu>
2017-10-22 19:40:27 -0700

    New zombie outfit

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -9,7 +9,7 @@ public class Zombie : MonoBehaviour
     int amountOfBurgers, amountOfFries, amountOfDrinks;
     bool orderComplete;
     List<GameObject> onPlatter = new List<GameObject>();
-    GameObject head, thinkBubble, hair, leftForearm, rightForearm;
+    GameObject head, thinkBubble, hair, leftForearm, rightForearm, upperBody;
     GameObject leftHand, rightHand, leftFoot, rightFoot, leftLeg, rightLeg;
     GameObject rightUpperArm, leftUpperArm, leftThigh, rightThigh, lowerBody;
 
@@ -36,11 +36,11 @@ public class Zombie : MonoBehaviour
         thinkBubble = transform.GetChild(0).gameObject;
         thinkBubble.AddComponent<IncreaseSize>();
         GetBodyParts(gameObject);
-		GetComponent<Animator>().Play("Walking");
+        GetComponent<Animator>().Play("Walking");
         GetComponent<Animator>().SetBool("Walking", true);
         GetComponent<Animator>().SetFloat("Speed", speed * animationSpeed);
         startingZ = head.transform.position.z;
-		WakeUp();
+        WakeUp();
         SetOrder();
     }
 
@@ -339,100 +339,97 @@ public class Zombie : MonoBehaviour
         }
     }
 
-	void GetBodyParts(GameObject part)
-	{
-		switch (part.name)
-		{
-			case "Head":
-				head = part.gameObject;
-				break;
-			case "Left_Forearm":
-				leftForearm = part.gameObject;
-				break;
-			case "Right_Forearm":
-				rightForearm = part.gameObject;
-				break;
-			case "Hair":
-				hair = part.gameObject;
-				break;
-			case "Left_Hand":
+    void GetBodyParts(GameObject part)
+    {
+        switch (part.name)
+        {
+            case "Head":
+                head = part.gameObject;
+                break;
+            case "Left_Forearm":
+                leftForearm = part.gameObject;
+                break;
+            case "Right_Forearm":
+                rightForearm = part.gameObject;
+                break;
+            case "Hair":
+                hair = part.gameObject;
+                break;
+            case "Left_Hand":
                 leftHand = part.gameObject;
-				break;
-			case "Right_Hand":
+                break;
+            case "Right_Hand":
                 rightHand = part.gameObject;
-				break;
-			case "Left_Foot":
+                break;
+            case "Left_Foot":
                 leftFoot = part.gameObject;
-				break;
-			case "Right_Foot":
+                break;
+            case "Right_Foot":
                 rightFoot = part.gameObject;
-				break;
-			case "Left_Leg":
+                break;
+            case "Left_Leg":
                 leftLeg = part.gameObject;
-				break;
-			case "Right_Leg":
+                break;
+            case "Right_Leg":
                 rightLeg = part.gameObject;
-				break;
-			case "Left_Upper_Arm":
+                break;
+            case "Left_Upper_Arm":
                 leftUpperArm = part.gameObject;
-				break;
-			case "Right_Upper_Arm":
+                break;
+            case "Right_Upper_Arm":
                 rightUpperArm = part.gameObject;
-				break;
-			case "Left_Thigh":
+                break;
+            case "Left_Thigh":
                 leftThigh = part.gameObject;
-				break;
-			case "Right_Thigh":
-				rightThigh = part.gameObject;
-				break;
-			case "Lower_Body":
+                break;
+            case "Right_Thigh":
+                rightThigh = part.gameObject;
+                break;
+            case "Lower_Body":
                 lowerBody = part.gameObject;
-				break;
-		}
-		if (part.transform.childCount == 0)
-		{
-			return;
-		}
-		for (int i = 0; i < part.transform.childCount; i++)
-		{
-			GetBodyParts(part.transform.GetChild(i).gameObject);
-		}
-	}
+                break;
+            case "Upper_Body":
+                upperBody = part.gameObject;
+                break;
+        }
+        if (part.transform.childCount == 0)
+        {
+            return;
+        }
+        for (int i = 0; i < part.transform.childCount; i++)
+        {
+            GetBodyParts(part.transform.GetChild(i).gameObject);
+        }
+    }
 
-    public void SetZombie(float s, Mesh h, Mesh lF, Mesh rF, Mesh rH, Mesh lH, Mesh lFoot, Mesh rFoot, Mesh lL, Mesh rL, Mesh rU, Mesh lU, Mesh lT, Mesh rT, Mesh headMesh, Mesh lB)
+    public void SetZombie(float s, Mesh h, Mesh lF, Mesh rF, Mesh rH, Mesh lH, Mesh lFoot, Mesh rFoot, 
+                          Mesh lL, Mesh rL, Mesh rU, Mesh lU, Mesh lT, Mesh rT, Mesh headMesh, Mesh lB, Mesh uB, Material outfit)
     {
         speed = s;
         originalSpeed = s;
-        Mesh newHair = Instantiate(h);
-        Mesh newLeftForearm = Instantiate(lF);
-        Mesh newRightForearm = Instantiate(rF);
-		Mesh newLeftHand = Instantiate(lH);
-		Mesh newRightHand = Instantiate(rH);
-        Mesh newLeftFoot = Instantiate(lFoot);
-        Mesh newRightFoot = Instantiate(rFoot);
-        Mesh newLeftLeg = Instantiate(lL);
-		Mesh newRightLeg = Instantiate(rL);
-        Mesh newLeftUpperArm = Instantiate(lU);
-        Mesh newRightUpperArm = Instantiate(rU);
-		Mesh newLeftThigh = Instantiate(lT);
-		Mesh newRightThigh = Instantiate(rT);
-        Mesh newHead = Instantiate(headMesh);
-        Mesh newLowerBody = Instantiate(lB);
-        hair.GetComponent<MeshFilter>().mesh = newHair;
-        leftForearm.GetComponent<MeshFilter>().mesh = newLeftForearm;
-        rightForearm.GetComponent<MeshFilter>().mesh = newRightForearm;
-        leftHand.GetComponent<MeshFilter>().mesh = newLeftHand;
-        rightHand.GetComponent<MeshFilter>().mesh = newRightHand;
-        leftFoot.GetComponent<MeshFilter>().mesh = newLeftFoot;
-        rightFoot.GetComponent<MeshFilter>().mesh = newRightFoot;
-        leftLeg.GetComponent<MeshFilter>().mesh = newLeftLeg;
-        rightLeg.GetComponent<MeshFilter>().mesh = newRightLeg;
-        leftUpperArm.GetComponent<MeshFilter>().mesh = newLeftUpperArm;
-        rightUpperArm.GetComponent<MeshFilter>().mesh = newRightUpperArm;
-        leftThigh.GetComponent<MeshFilter>().mesh = newLeftThigh;
-        rightThigh.GetComponent<MeshFilter>().mesh = newRightThigh;
-        head.GetComponent<MeshFilter>().mesh = newHead;
-        lowerBody.GetComponent<MeshFilter>().mesh = newLowerBody;
+        SetUpLimb(hair, h, outfit);
+        SetUpLimb(leftForearm, lF, outfit);
+        SetUpLimb(rightForearm, rF, outfit);
+        SetUpLimb(leftHand, lH, outfit);
+        SetUpLimb(rightHand, rH, outfit);
+        SetUpLimb(leftFoot, lFoot, outfit);
+        SetUpLimb(rightFoot, rFoot, outfit);
+        SetUpLimb(leftLeg, lL, outfit);
+        SetUpLimb(rightLeg, rL, outfit);
+        SetUpLimb(leftUpperArm, lU, outfit);
+        SetUpLimb(rightUpperArm, rU, outfit);
+        SetUpLimb(rightForearm, rF, outfit);
+        SetUpLimb(leftThigh, lT, outfit);
+        SetUpLimb(rightThigh, rT, outfit);
+        SetUpLimb(head, headMesh, outfit);
+        SetUpLimb(lowerBody, lB, outfit);
+        SetUpLimb(upperBody, uB, outfit);
+    }
+
+    void SetUpLimb(GameObject limb, Mesh newMesh, Material outfit)
+    {
+        limb.GetComponent<MeshFilter>().mesh = Instantiate(newMesh);
+        limb.GetComponent<Renderer>().material = outfit;
     }
 
     void SetUpSprites()
--- Assets/Scripts/ZombieManager.cs
+++ Assets/Scripts/ZombieManager.cs
@@ -18,9 +18,11 @@ public class ZombieManager : MonoBehaviour
     public Mesh[] legs;
     public Mesh[] rightUpperArms;
     public Mesh[] leftUpperArms;
-	public Mesh[] thighs;
+    public Mesh[] thighs;
     public Mesh[] heads;
     public Mesh[] lowerBody;
+    public Mesh[] upperBody;
+    public Material[] outfit;
 
     void Start()
     {
@@ -33,25 +35,27 @@ public class ZombieManager : MonoBehaviour
         amountOfWaiters++;
         GameObject newWaiter = Instantiate(zombie);
         newWaiter.transform.position = position;
-		newWaiter.transform.LookAt(gameplayPosition);
-		newWaiter.transform.eulerAngles = new Vector3(0, newWaiter.transform.eulerAngles.y, newWaiter.transform.eulerAngles.z);
+        newWaiter.transform.LookAt(gameplayPosition);
+        newWaiter.transform.eulerAngles = new Vector3(0, newWaiter.transform.eulerAngles.y, newWaiter.transform.eulerAngles.z);
         newWaiter.AddComponent<Zombie>().SetZombie(
-            1, 
-            hair[Random.Range(0, hair.Length)], 
-            forearm[Random.Range(0, forearm.Length)], 
+            1,
+            hair[Random.Range(0, hair.Length)],
+            forearm[Random.Range(0, forearm.Length)],
             forearm[Random.Range(0, forearm.Length)],
             hands[Random.Range(0, hands.Length)],
             hands[Random.Range(0, hands.Length)],
             feet[Random.Range(0, feet.Length)],
             feet[Random.Range(0, feet.Length)],
-			legs[Random.Range(0, legs.Length)],
-			legs[Random.Range(0, legs.Length)],
+            legs[Random.Range(0, legs.Length)],
+            legs[Random.Range(0, legs.Length)],
             rightUpperArms[Random.Range(0, rightUpperArms.Length)],
             leftUpperArms[Random.Range(0, leftUpperArms.Length)],
             thighs[Random.Range(0, thighs.Length)],
             thighs[Random.Range(0, thighs.Length)],
             heads[Random.Range(0, heads.Length)],
-            lowerBody[Random.Range(0, lowerBody.Length)]
+            lowerBody[Random.Range(0, lowerBody.Length)],
+            upperBody[Random.Range(0, upperBody.Length)],
+            outfit[Random.Range(0, outfit.Length)]
         );
         newWaiter.tag = "Clone";
     }
