6d1f2802a29f00dae8a097f9993578a601061117
Brandon Gomez <brlgomez@ucsc.edu>
2017-12-06 14:39:29 -0800

    Removed standard assets

--- Assets/Scripts/GraphicsManager.cs
+++ Assets/Scripts/GraphicsManager.cs
@@ -7,6 +7,7 @@ public class GraphicsManager : MonoBehaviour
     public List<Sprite> graphicSprites;
     public List<Graphic> graphicList = new List<Graphic>();
     public Graphic normal, classic, classicColor, blackAndWhite, noir;
+    public Texture2D classicTexture, blueAndGreenTexture;
 
     public class Graphic
     {
@@ -31,9 +32,9 @@ public class GraphicsManager : MonoBehaviour
         PlayerPrefs.SetInt("Graphic 0", 1);
         graphicList.Add(normal = new Graphic(0, 0, "Normal", graphicSprites[0]));
         graphicList.Add(classic = new Graphic(1, 0, "Classic", graphicSprites[1]));
-        graphicList.Add(classicColor = new Graphic(2, 10, "Classic color", graphicSprites[2]));
-        graphicList.Add(blackAndWhite = new Graphic(3, 20, "Black & white", graphicSprites[3]));
-        graphicList.Add(noir = new Graphic(4, 30, "Noir", graphicSprites[4]));
+        graphicList.Add(classicColor = new Graphic(2, 10, "Blue & Green", graphicSprites[2]));
+        graphicList.Add(blackAndWhite = new Graphic(3, 20, "", graphicSprites[3]));
+        graphicList.Add(noir = new Graphic(4, 30, "", graphicSprites[4]));
         SetGraphic(PlayerPrefs.GetInt("GRAPHICS"));
     }
 
@@ -46,7 +47,13 @@ public class GraphicsManager : MonoBehaviour
         else if (graphicNum == 1)
         {
             GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
-        }
+			GetComponent<Assets.Pixelation.Scripts.Chunky>().SprTex = classicTexture;
+		}
+        else if (graphicNum == 2)
+		{
+			GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
+            GetComponent<Assets.Pixelation.Scripts.Chunky>().SprTex = blueAndGreenTexture;
+		}
         else
         {
             GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = false;
