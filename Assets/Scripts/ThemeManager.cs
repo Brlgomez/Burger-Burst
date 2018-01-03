618815118442f52a75f68f95f2af77844c0545c6
Brandon Gomez <brlgomez@ucsc.edu>
2018-01-02 21:04:12 -0800

    New 70s theme

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -52,8 +52,9 @@ public class ThemeManager : MonoBehaviour
         flooringList.Add(new CustomItem(5, 50, PlayerPrefsManager.specificFlooring, "Minimal Floor", flooringSprites[5], flooringTexture[5]));
         flooringList.Add(new CustomItem(6, 50, PlayerPrefsManager.specificFlooring, "Pancake Tuesday\nFloor", flooringSprites[6], flooringTexture[6]));
         flooringList.Add(new CustomItem(7, 50, PlayerPrefsManager.specificFlooring, "Bubbles Floor", flooringSprites[7], flooringTexture[7]));
+		flooringList.Add(new CustomItem(8, 50, PlayerPrefsManager.specificFlooring, "Groovy Floor", flooringSprites[8], flooringTexture[8]));
 
-        wallList.Add(new CustomItem(0, 0, PlayerPrefsManager.specificWallpaper, "C&B Wallpaper", wallSprites[0], wallTexture[0]));
+		wallList.Add(new CustomItem(0, 0, PlayerPrefsManager.specificWallpaper, "C&B Wallpaper", wallSprites[0], wallTexture[0]));
         wallList.Add(new CustomItem(1, 50, PlayerPrefsManager.specificWallpaper, "Woodland Wallpaper", wallSprites[1], wallTexture[1]));
         wallList.Add(new CustomItem(2, 50, PlayerPrefsManager.specificWallpaper, "Cloudy Sky Wallpaper", wallSprites[2], wallTexture[2]));
         wallList.Add(new CustomItem(3, 50, PlayerPrefsManager.specificWallpaper, "Ceramic Wallpaper", wallSprites[3], wallTexture[3]));
@@ -61,8 +62,9 @@ public class ThemeManager : MonoBehaviour
         wallList.Add(new CustomItem(5, 50, PlayerPrefsManager.specificWallpaper, "Minimal Wallpaper", wallSprites[5], wallTexture[5]));
         wallList.Add(new CustomItem(6, 50, PlayerPrefsManager.specificWallpaper, "Pancake Tuesday\nWallpaper", wallSprites[6], wallTexture[6]));
         wallList.Add(new CustomItem(7, 50, PlayerPrefsManager.specificWallpaper, "Electric Fish Wallpaper", wallSprites[7], wallTexture[7]));
+		wallList.Add(new CustomItem(8, 50, PlayerPrefsManager.specificWallpaper, "Grovy Wallpaper", wallSprites[8], wallTexture[8]));
 
-        detailList.Add(new CustomItem(0, 0, PlayerPrefsManager.specificDetail, "C&B Detail", detailSprites[0], detailTexture[0]));
+		detailList.Add(new CustomItem(0, 0, PlayerPrefsManager.specificDetail, "C&B Detail", detailSprites[0], detailTexture[0]));
         detailList.Add(new CustomItem(1, 25, PlayerPrefsManager.specificDetail, "Woodland Detail", detailSprites[1], detailTexture[1]));
         detailList.Add(new CustomItem(2, 25, PlayerPrefsManager.specificDetail, "Outdoors Detail", detailSprites[2], detailTexture[2]));
         detailList.Add(new CustomItem(3, 25, PlayerPrefsManager.specificDetail, "Ceramic Detail", detailSprites[3], detailTexture[3]));
@@ -70,8 +72,9 @@ public class ThemeManager : MonoBehaviour
         detailList.Add(new CustomItem(5, 25, PlayerPrefsManager.specificDetail, "Minimal Detail", detailSprites[5], detailTexture[5]));
         detailList.Add(new CustomItem(6, 25, PlayerPrefsManager.specificDetail, "Pancake Tuesday\nDetail", detailSprites[6], detailTexture[6]));
         detailList.Add(new CustomItem(7, 25, PlayerPrefsManager.specificDetail, "Electric Blue Detail", detailSprites[7], detailTexture[7]));
+		detailList.Add(new CustomItem(8, 25, PlayerPrefsManager.specificDetail, "Groovy Detail", detailSprites[8], detailTexture[8]));
 
-        SetFlooring(GetComponent<PlayerPrefsManager>().GetFlooring());
+		SetFlooring(GetComponent<PlayerPrefsManager>().GetFlooring());
         SetWallpaper(GetComponent<PlayerPrefsManager>().GetWallpaper());
         SetDetail(GetComponent<PlayerPrefsManager>().GetDetail());
     }
