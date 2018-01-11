9de82e21ab28813b94660af7c1258a804f2137ad
Brandon Gomez <brlgomez@ucsc.edu>
2018-01-11 15:53:02 -0800

    Added new graphics

--- Assets/Scripts/GraphicsManager.cs
+++ Assets/Scripts/GraphicsManager.cs
@@ -6,7 +6,7 @@ public class GraphicsManager : MonoBehaviour
 {
     public List<Sprite> graphicSprites;
     public List<Graphic> graphicList = new List<Graphic>();
-    public Texture2D classicTexture, blueAndGreenTexture, greenAndOrange;
+    public Texture2D classicTexture, blueAndGreenTexture, greenAndOrange, redRetro, rainbow, grapeFruit;
 
     public class Graphic
     {
@@ -30,11 +30,14 @@ public class GraphicsManager : MonoBehaviour
     {
         PlayerPrefs.SetInt(PlayerPrefsManager.specificGraphics + 0, 1);
         graphicList.Add(new Graphic(0, 0, "Normal", graphicSprites[0]));
-        graphicList.Add(new Graphic(1, 0, "Classic", graphicSprites[1]));
-        graphicList.Add(new Graphic(2, 10, "Blue & Green", graphicSprites[2]));
-        graphicList.Add(new Graphic(3, 20, "Green & Orange", graphicSprites[3]));
-        graphicList.Add(new Graphic(4, 30, "Pixelated", graphicSprites[4]));
-		graphicList.Add(new Graphic(5, 30, "Outlined", graphicSprites[5]));
+        graphicList.Add(new Graphic(1, 100, "Retro", graphicSprites[1]));
+        graphicList.Add(new Graphic(2, 100, "Blue & Green", graphicSprites[2]));
+        graphicList.Add(new Graphic(3, 100, "Green & Orange", graphicSprites[3]));
+        graphicList.Add(new Graphic(4, 100, "Pixelated", graphicSprites[4]));
+        graphicList.Add(new Graphic(5, 100, "Outlined", graphicSprites[5]));
+		graphicList.Add(new Graphic(6, 100, "Red Retro", graphicSprites[6]));
+		graphicList.Add(new Graphic(7, 100, "Grape Fruit", graphicSprites[7]));
+		graphicList.Add(new Graphic(8, 100, "Lucid Dreaming", graphicSprites[8]));
 		SetGraphic(GetComponent<PlayerPrefsManager>().GetGraphics());
     }
 
@@ -46,7 +49,7 @@ public class GraphicsManager : MonoBehaviour
                 GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = false;
                 GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
                 GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = false;
-				break;
+                break;
             case 1:
                 GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
                 GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
@@ -70,11 +73,29 @@ public class GraphicsManager : MonoBehaviour
                 GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = true;
                 GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = false;
                 break;
-			case 5:
-				GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = false;
+            case 5:
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = false;
                 GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
                 GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = true;
-				break;
+                break;
+            case 6:
+                GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().SprTex = redRetro;
+                GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = false;
+                break;
+            case 7:
+                GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().SprTex = grapeFruit;
+                GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = false;
+                break;
+            case 8:
+                GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = true;
+                GetComponent<Assets.Pixelation.Scripts.Chunky>().SprTex = rainbow;
+                GetComponent<UnityStandardAssets.ImageEffects.EdgeDetection>().enabled = false;
+                break;
             default:
                 GetComponent<Assets.Pixelation.Scripts.Pixelation>().enabled = false;
                 GetComponent<Assets.Pixelation.Scripts.Chunky>().enabled = false;
